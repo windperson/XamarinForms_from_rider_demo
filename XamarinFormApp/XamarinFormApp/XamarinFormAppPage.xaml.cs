35b27780d35e2b8aa7f8fc27339308e8ec3b648e
Chen Yu Pao <windperson@gmail.com>
2018-05-30 15:53:40 +0800

    Demo code created from JetBrains Rider

---
+++ XamarinFormApp/XamarinFormApp/XamarinFormAppPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinFormApp
+{
+    public partial class XamarinFormAppPage : ContentPage
+    {
+        public XamarinFormAppPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("button clicked!");
+        }
+    }
+}
